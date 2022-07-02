788d7fcf5974b1b2b2db42d93178ecd78c7c89e4
Sanni Oluwadamilola <sannijohnson5@gmail.com>
2022-07-02 18:03:56 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const router = express.Router()
+const bodyparser = require('body-parser')
+
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json())
+app.use('/', router)
+
+const computeData = require('./computeData')
+
+router.get('/', (req, res)=>{
+    res.send("Stuff")
+})
+
+router.post("/splits-payment/compute", (req, res)=>{
+
+    const {ID, Amount, SplitInfo} = req.body
+    const result = computeData({ID, Amount, SplitInfo})
+    res.status(200).send(result)
+})
+
+
+app.listen(3000, ()=>{
+    console.log('Server listening in on port 3000...')
+})
